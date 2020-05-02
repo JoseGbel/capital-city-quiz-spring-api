3e415f1ba5f2cd51c532ad07e18b547f984e0285
Jose Garcia <jose.gbel@gmail.com>
2020-05-02 21:17:52 +0100

    added email and username uniqueness

--- src/main/kotlin/sample/kotlin/spring/user/ApplicationUser.kt
+++ src/main/kotlin/sample/kotlin/spring/user/ApplicationUser.kt
@@ -1,19 +1,18 @@
 package sample.kotlin.spring.user
 
 import org.springframework.data.jpa.domain.AbstractPersistable
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 import javax.validation.constraints.NotEmpty
 
 @Entity
 class ApplicationUser (
     @NotEmpty
+    @Column(unique=true)
     val username: String,
     @NotEmpty
     var password: String,
     @NotEmpty
+    @Column(unique=true)
     val email: String,
     @NotEmpty
     val name: String,
--- src/main/kotlin/sample/kotlin/spring/user/UserController.kt
+++ src/main/kotlin/sample/kotlin/spring/user/UserController.kt
@@ -1,10 +1,13 @@
 package sample.kotlin.spring.user
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.lang.IllegalArgumentException
 
 @RestController
 @RequestMapping("/users")
@@ -12,8 +15,14 @@ class UserController(private val applicationUserRepository: ApplicationUserRepos
                      private val bCryptPasswordEncoder: BCryptPasswordEncoder) {
 
     @PostMapping("/sign-up")
-    fun signUp(@RequestBody user: ApplicationUser) {
+    fun signUp(@RequestBody user: ApplicationUser) : ResponseEntity<Boolean> {
         user.password = bCryptPasswordEncoder.encode(user.password)
-        applicationUserRepository.save(user)
+        try{
+            applicationUserRepository.save(user)
+        } catch (e: IllegalArgumentException){
+            return ResponseEntity.badRequest().body(false)
+        }
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(true)
     }
 }
